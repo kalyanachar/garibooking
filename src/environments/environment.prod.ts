ca412a0cccabfc863bfc7010f065426541b1f1fd
Kalyan <kalyanachar@gmail.com>
2019-12-27 23:09:43 +0530

    updated code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // apiEndpoint: "http://132.148.130.125/mach_mangso_more/api/",
   // imageBaseUrl: "http://132.148.130.125/mach_mangso_more/uploads/"
-  apiEndpoint: "http://garibooking.com/webservice/",
- imageBaseUrl: "http://garibooking.com/webservice/uploads/"
+  apiEndpoint: "https://garibooking.com/webservice/",
+ imageBaseUrl: "https://garibooking.com/webservice/uploads/"
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
  // apiEndpoint: "http://jistechnologies.com/kalyan/carrental/webservice/",
  // imageBaseUrl: "http://jistechnologies.com/kalyan/carrental/webservice/uploads/"
- apiEndpoint: "http://garibooking.com/webservice/",
- imageBaseUrl: "http://garibooking.com/webservice/uploads/"
+ apiEndpoint: "https://garibooking.com/webservice/",
+ imageBaseUrl: "https://garibooking.com/webservice/uploads/"
  
 };
